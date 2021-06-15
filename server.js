6f5b8a759fb817b94539bc69c7c439f2ccd80e46
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-15 14:39:54 +0530

    basic express api server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    app: "Expense Tracker",
+    creator: "Somsubhra Das",
+  });
+});
+
+app.use("/api/v1/transactions", require("./api/transactions"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
