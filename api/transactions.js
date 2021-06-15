34e3702d7750f7796325a98c3800fec997eb58dd
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-15 15:01:44 +0530

    get and post api done

--- api/transactions.js
+++ api/transactions.js
@@ -1,3 +1,27 @@
 const router = require("express").Router();
 
+const { PrismaClient } = require("@prisma/client");
+const { transaction: Transaction } = new PrismaClient();
+
+router.get("/", async (req, res) => {
+  const transactions = await Transaction.findMany();
+
+  res.json({ success: true, count: transactions.length, data: transactions });
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { text, amount } = req.body;
+    const newTransaction = await Transaction.create({
+      data: {
+        text,
+        amount,
+      },
+    });
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, error: ["Validation Error"] });
+  }
+});
+
 module.exports = router;
